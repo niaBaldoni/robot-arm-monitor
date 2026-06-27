38323504f1dda3452089bbec493bbc51b9e8fd6e
niaBaldoni <16371386+niaBaldoni@users.noreply.github.com>
2026-06-27 17:58:53 +0200

    migrated from std::thread to std::jthread, closes #19

--- src/main.cpp
+++ src/main.cpp
@@ -20,19 +20,12 @@ void displayThread() {
 }
 
 int main() {
-    std::thread t0(armSimulatorThread);
-    std::thread t1(encoderThread);
-    std::thread t2(temperatureThread);
-    std::thread t3(forceThread);
-    std::thread t4(displayThread);
-    std::thread t5(monitorThread);
-
-    t0.join();
-    t1.join();
-    t2.join();
-    t3.join();
-    t4.join();
-    t5.join();
+    std::jthread t0(armSimulatorThread);
+    std::jthread t1(encoderThread);
+    std::jthread t2(temperatureThread);
+    std::jthread t3(forceThread);
+    std::jthread t4(displayThread);
+    std::jthread t5(monitorThread);
 
     return 0;
 }
